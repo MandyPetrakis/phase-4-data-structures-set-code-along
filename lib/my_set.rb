1a36c7273032875034f0b8540709c47ad19c95e9
Mandy Petrakis <mandypetrakis@gmail.com>
2023-09-03 19:09:40 -0700

    commit tree 812e15e7e0c5d16b36c5f5826018c22303eed2f6

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,44 @@
-# your code here
+class MySet
+    attr_accessor :hash
+
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true 
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true 
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size 
+        @hash.size
+    end
+
+    def clear
+        @hash={}
+        self
+    end
+
+    def each(block)
+        @hash.each(block)
+        self
+    end
+
+    def inspect
+        puts self
+    end
+
+end
+
